cf51ded5b56f74b7a833c5be9d061b486e9cc993
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2021-01-09 17:30:04 +0200

    Add files via upload

---
+++ URI Online Judge/Distance.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+ 
+        int d, time;
+	cin >> d;
+	time = (60 * d) / 30;
+	cout << time << " " << "minutos" << endl;
+ 
+    return 0;
+}
