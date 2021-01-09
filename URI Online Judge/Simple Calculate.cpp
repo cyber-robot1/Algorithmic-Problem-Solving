128a79e39ae7b3a0f5c61b3f2b04fc5b611c6adc
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2021-01-09 07:15:03 -0800

    Create Simple Calculate.cpp

---
+++ URI Online Judge/Simple Calculate.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<iomanip>
+using namespace std;
+ 
+int main() {
+ 
+   	int A, B, C, D;
+	  float X, Y, Z;
+	  cin >> A >> B >> X;
+	  cin >> C >> D >> Y;
+	  Z = (B * X) + (D * Y);
+	  cout << fixed << setprecision(2) << "VALOR A PAGAR: R$ " << Z << endl;
+
+
+ 
+    return 0;
+}
