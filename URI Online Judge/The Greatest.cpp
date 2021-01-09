d7b34732f7b51cbebe5abb5535feeeec483bd940
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2021-01-09 07:21:39 -0800

    Create The Greatest.cpp

---
+++ URI Online Judge/The Greatest.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+ 
+int main() {
+ 
+  int a, b, c, max;
+	cin >> a >> b >> c;
+	max = (a + b + abs(a - b)) / 2;
+	max = (max + c + abs(max - c)) / 2;
+	cout << max << " " << "eh o maior" << endl;
+ 
+    return 0;
+}
