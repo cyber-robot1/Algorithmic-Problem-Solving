8c731d90aa1fb6889b6415b8bcb047b3e14c306b
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2021-01-09 18:03:32 +0200

    Add files via upload

---
+++ URI Online Judge/Even Numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+ 
+        for (int k = 1; k <= 100; k++) 
+	{
+		if (k % 2 == 0)
+		{
+			cout << k << endl;
+		}
+	}
+ 
+    return 0;
+}
