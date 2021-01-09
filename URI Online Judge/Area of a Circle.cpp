a0f0119e65bc5a7a0df2657d31fdc2813286acd5
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2021-01-09 17:04:26 +0200

    Add files via upload

---
+++ URI Online Judge/Area of a Circle.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main() {
+ 
+        double R, n = 3.14159;
+	cin >> fixed >> setprecision(2) >> R;
+	double A = n * pow(R, 2);
+	cout << fixed << setprecision(4) << "A=" << A << endl;
+ 
+    return 0;
+}
