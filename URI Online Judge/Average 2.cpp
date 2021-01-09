213bca84d879d3ab0e827836402d0fe303d6ab5e
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2021-01-09 07:10:59 -0800

    Create Average 2.cpp

---
+++ URI Online Judge/Average 2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<iomanip>
+using namespace std;
+int main() {
+   double A, B, C, MEDIA;
+	 cin >> fixed >> setprecision(1) >> A >> B >> C;
+	 A *= 2;
+	 B *= 3;
+	 C *= 5;
+	 MEDIA = (A + B + C) / 10;
+	 cout << fixed << setprecision(1) << "MEDIA = " << MEDIA << endl;
+    
+    return 0;
+}
