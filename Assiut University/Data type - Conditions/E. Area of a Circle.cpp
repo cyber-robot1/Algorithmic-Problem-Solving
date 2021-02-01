b3123c78fa5b98f5df52eda9a2c2c1e9ac3b7925
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2021-02-01 03:29:00 -0800

    Create E. Area of a Circle.cpp

---
+++ Assiut University/Data type - Conditions/E. Area of a Circle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+    
+    double R,Area;
+    cin>>R;
+    Area= 3.141592653*R*R;
+    cout << fixed << setprecision(9)<<Area<<endl;
+     
+}
