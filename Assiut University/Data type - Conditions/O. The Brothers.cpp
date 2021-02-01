6b54bafec5d1c789c3ab846726a61a3e100e0fcc
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2021-02-01 06:52:36 -0800

    Create O. The Brothers.cpp

---
+++ Assiut University/Data type - Conditions/O. The Brothers.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    
+    string S1,F1,S2,F2;
+    cin>>S1>>F1>>S2>>F2;
+    if(F1==F2)
+    cout<<"ARE Brothers"<<endl;
+    else
+    cout<<"NOT"<<endl;
+   
+    return 0;
+}
