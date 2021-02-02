d612ba7f123bccbfa391f70201dba8b013c5d0f7
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2021-02-02 10:11:17 -0800

    Create I. Divisors.cpp

---
+++ Assiut University/Loops/I. Divisors.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    long long N;
+    cin>>N;
+    for(int i=1; i<=N; i++)
+    {
+        if(N%i==0)
+        cout<<i<<endl;
+    }
+   
+    return 0;
+}
