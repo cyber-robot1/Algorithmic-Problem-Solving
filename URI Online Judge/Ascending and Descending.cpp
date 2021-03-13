36aa9eda6db3377a8d6bf3acdf00df36592395a9
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2021-03-13 11:05:31 +0200

    Create Ascending and Descending.cpp

---
+++ URI Online Judge/Ascending and Descending.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int X,Y;
+    while(true)
+    {
+        cin>>X>>Y;
+        if(X==Y)
+            break;
+        else
+        {
+            if(X<Y)
+                cout<<"Crescente"<<endl;
+            else
+                cout<<"Decrescente"<<endl;
+        }
+    }
+
+    return 0;
+}
+
+
+
