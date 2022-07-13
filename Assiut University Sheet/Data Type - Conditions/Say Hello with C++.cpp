217371fbc293b9c4295ac12c4060198b3185b748
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2022-07-13 02:30:25 +0200

    Update Say Hello with C++.cpp

--- Assiut University Sheet/Data Type - Conditions/Say Hello with C++.cpp
+++ Assiut University Sheet/Data Type - Conditions/Say Hello with C++.cpp
@@ -2,10 +2,8 @@
 using namespace std;
 int main()
 {
-    long long X, Y;
-    cin >> X >> Y;
-    cout << X << " + " << Y << " = " << X + Y << endl;
-    cout << X << " * " << Y << " = " << X * Y << endl;
-    cout << X << " - " << Y << " = " << X - Y << endl;
+    string S;
+    cin >> S;
+    cout << "Hello, " << S << endl;
 }
 
