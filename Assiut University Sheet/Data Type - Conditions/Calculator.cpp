393e82f6621a608200f514284868d338746a1674
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2022-07-19 18:29:57 +0200

    Create Calculator.cpp

---
+++ Assiut University Sheet/Data Type - Conditions/Calculator.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    long long A, B;
+    char S;
+    cin >> A >> S >> B;
+    if (S == '+')
+    {
+        cout << A + B << endl;
+    }
+    else if (S == '-')
+    {
+        cout << A - B << endl;
+    }
+    else if (S == '*')
+    {
+        cout << A * B << endl;
+    }
+    else if (S == '/')
+    {
+        cout << A / B << endl;
+    }
+}
