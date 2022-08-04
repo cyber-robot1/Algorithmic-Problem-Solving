499fdf72b12a77ec4f937c8fed3f56c82aaae7d8
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2022-08-04 11:23:52 +0200

    Update LCM.cpp

--- Implementation/LCM.cpp
+++ Implementation/LCM.cpp
@@ -1,39 +1,43 @@
 /*
 Author: Karim Tarek Ibrahim
 */
-// this is an algorithm to calculate the LCM (Least Common Multiple) of two numbers and print it. 
+// this is a function to calculate the LCM (Least Common Multiple) of two numbers and print it. 
 #include <iostream>
 using namespace std;
-
-int main()
+int lcm(int x, int y)
 {
-	int n, m, lcm = 0, max = 0;
-	cin >> n >> m;
-	if (n == 0 || m == 0)
-		cout << 0 << endl;
+	int lcm, max = 0;
+	if (x == 0 || y == 0)
+		cout << 0;
 	else
 	{
-		if (n > m)
+		if (x > y)
 		{
-			max = n;
-			lcm = n;
+			max = x;
+			lcm = x;
 		}
 		else
 		{
-			max = m;
-			lcm = m;
+			max = y;
+			lcm = y;
 		}
 		while (true)
 		{
-			if (lcm % n == 0 && lcm % m == 0)
+			if (lcm % x == 0 && lcm % y == 0)
 			{
-				cout << "lcm(" << n << "," << m << ")" << " = " << lcm << endl;
+				return lcm;
 				break;
 			}
 			else
 				lcm += max;
 		}
 	}
-    
-  return 0;
+}
+int main()
+{	
+	int n, m;
+	cin >> n >> m;
+	cout << lcm(n, m);
+	
+    return 0;
 }
