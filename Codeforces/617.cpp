01203bc32254df9bfcf6aadb541521035bc65652
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2022-08-08 03:06:19 +0200

    Update 617.cpp

--- Codeforces/617.cpp
+++ Codeforces/617.cpp
@@ -1,12 +1,15 @@
+/*
+Author: Karim Tarek Ibrahim
+*/
 #include <iostream>
 using namespace std;
 
 int main()
 {
 	int x;
-	int steps = 0;    // flag variable
+	int steps = 0;                                               // flag variable.
 	cin >> x;
-	if (x == 1 || x == 2 || x == 3 || x == 4 || x == 5)  // base case
+	if (x == 1 || x == 2 || x == 3 || x == 4 || x == 5)        // base case.
 		cout << 1 << endl;
 	else if (x > 5)
 	{
