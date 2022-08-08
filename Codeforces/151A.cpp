5f283c1d5190ce239226c9ae901dfd3b422d0515
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2022-08-08 03:02:02 +0200

    Create 151A.cpp

---
+++ Codeforces/151A.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int min(int x, int y, int z)
+{
+	int min = 0;
+	if (x <= y && x <= z)
+		min = x;
+	else if (y <= x && y <= z)
+		min = y;
+	else if (z <= x && z <= y)
+		min = z;
+	return min;
+}
+int main()
+{
+	
+	int n, k, l, c, d, p, nl, np, t_limes = 0, t_drink = 0, t_salt = 0;
+	cin >> n >> k >> l >> c >> d >> p >> nl >> np;
+	t_limes = c * d;                                                       // total limes for each friend.
+	t_drink = (k * l) / nl;                                               // total amount of drink for each friend.
+	t_salt = p / np;                                                     // total amount of salt for each friend.
+	cout << min(t_limes, t_drink, t_salt) / n;
+
+
+	return 0;
+}
