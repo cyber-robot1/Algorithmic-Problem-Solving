558793a96e2747426f88d328c1fd405cd6ec4e6b
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2022-08-08 03:05:08 +0200

    Update 546A.cpp

--- Codeforces/546A.cpp
+++ Codeforces/546A.cpp
@@ -6,8 +6,8 @@ using namespace std;
  
 int main()
 {
-	int k, w;          // k is cost of first banana, w is number of banana the soldier wants
-	long long n, total_price = 0; // n is the number of dollars the soldier has
+	int k, w;                                  // k is cost of first banana, w is number of banana the soldier wants.
+	long long n, total_price = 0;             // n is the number of dollars the soldier has.
 	cin >> k >> n >> w;
 	for (int i = 1; i <= w; i++)
 	{
