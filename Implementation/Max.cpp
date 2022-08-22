a680ac97298b66897825ec9b95eeeabeda50f5a1
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2022-08-22 16:02:26 +0200

    Create Max.cpp

---
+++ Implementation/Max.cpp
@@ -0,0 +1,30 @@
+/*
+Author: Karim Tarek Ibrahim
+This's a function of three parameters to determine the max among them.
+*/
+#include <iostream>
+using namespace std;
+
+int Max(int a, int b, int c)
+{
+	int mx = 0;
+	if (a > b && a > c)
+		mx = a;
+	else if (b > a && b > c)
+		mx = b;
+	else if (c > a && c > b)
+		mx = c;
+	return mx;
+}
+
+int main()
+{
+	int x, y, z;
+	cin >> x >> y >> z;
+  cout << Max(x,y,z);
+		
+	
+	
+		return 0;
+}
+
