cbdab3c674318b8f484999f4890ac44fab681ea7
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2022-08-22 16:15:15 +0200

    Create Mid.cpp

---
+++ Implementation/Mid.cpp
@@ -0,0 +1,29 @@
+/*
+Author: Karim Tarek Ibrahim
+This's a function of three parameters to determine the middle among them.
+*/
+
+#include <iostream>
+using namespace std;
+
+int Mid(int a, int b, int c)
+{
+	int md = 0;
+	if (b <= a && a <= c || c <= a && a <= b)
+		md = a;
+	else if (a <= b && b <= c || c <= b && b <= a)
+		md = b;
+	else
+		md = c;
+	return md;
+}
+
+int main()
+{
+	int x, y, z;
+	cin >> x >> y >> z;
+  cout << Mid(x,y,z);
+		
+	
+	return 0;
+}
