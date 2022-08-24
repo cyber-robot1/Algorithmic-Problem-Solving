10dfb18219ab7992a6936ff9ebd6c792422b9446
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2022-08-25 00:43:56 +0200

    Create 746A.cpp

---
+++ Codeforces/746A.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int a, b, c, flag = 0;
+	cin >> a >> b >> c;
+	if (!(a >= 1 && b >= 2 && c >= 4))
+		cout << 0;
+	else
+	{
+		while (a >= 1 && b >= 2 && c >= 4)
+		{
+			a -= 1;
+			b -= 2;
+			c -= 4;
+			flag++;
+		}
+		cout << 7 * flag;
+	}
+	
+		
+	return 0;
+}
+
