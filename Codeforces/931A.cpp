dbf59b6c306afb564fe30312dc579da95d1ebf39
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2022-08-30 21:42:39 +0200

    Update 931A.cpp

--- Codeforces/931A.cpp
+++ Codeforces/931A.cpp
@@ -10,10 +10,9 @@ int main()
 {
 	int a, b, num_steps = 0, N = 0, result = 0;
 	cin >> a >> b;
-	if (a > b)
-		num_steps = a - b;
-	else
-		num_steps = b - a;
+	
+	num_steps = abs(a - b);
+
 	if (num_steps % 2 == 0)      // even 
 	{
 		N = num_steps / 2;
@@ -21,15 +20,12 @@ int main()
 	}
 	else                        // odd
 	{
-		N = num_steps / 2;        
+		N = num_steps / 2;
 		result = (N * (N + 1)) + ceil(double(num_steps) / 2);
 	}
 	cout << result;
 
 
-		
+
 	return 0;
 }
-
-
-
