1f2ba3a81c02f73bfb5f4b68c7b4cd27bb2226d8
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2022-09-02 03:40:12 +0200

    Create 749.cpp

---
+++ Codeforces/749.cpp
@@ -0,0 +1,40 @@
+/*
+Author: Karim Tarek Ibrahim
+Date: 9/2/2022
+Problem name: Bachgold Problem
+Problem link: https://codeforces.com/problemset/problem/749/A
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	
+	int n, k = 0;
+	cin >> n;
+	if (n % 2 == 0)               // n is even 
+	{
+		k = n / 2;
+		cout << k << endl;
+		for (int i = 1; i <= k; i++)
+		{
+			cout << 2 << " ";
+		}
+	}
+	else                          // n is odd
+	{
+		k = n / 2;
+		cout << k << endl;
+		for (int i = 1; i <= ((n / 2) - 1); i++)
+		{
+			cout << 2 << " ";
+		}
+		for (int i = 1; i <= k - ((n / 2) - 1); i++)
+		{
+			cout << 3 << " ";
+		}
+	}
+
+	return 0;
+}
