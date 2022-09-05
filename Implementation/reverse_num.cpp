ef67cea0a7fc24b8a7e52dc0d9f6fa698f1b8a18
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2022-09-06 00:08:28 +0200

    Create reverse_num.cpp

---
+++ Implementation/reverse_num.cpp
@@ -0,0 +1,29 @@
+/*
+Author: Karim Tarek Ibrahim
+*/
+// This is a function to reverse a given number.
+
+#include <iostream>
+using namespace std;
+
+int reverse_num(int n)
+{
+	int rev_n = 0;                       // rev_n = reverse of n.
+	for (int i = 0; n != 0; i++)
+	{
+		int digit = n % 10;
+		n = n / 10;
+		rev_n = (rev_n * 10) + digit;
+	}
+	return rev_n;
+}
+
+int main()
+{
+	
+  int x;
+	cin >> x;
+	cout << reverse_num(x);
+
+	return 0;
+}
