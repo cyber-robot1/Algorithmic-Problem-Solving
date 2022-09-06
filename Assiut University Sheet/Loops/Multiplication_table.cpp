0380e1387fa9982deb8e2c68c851e442dff04a22
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2022-09-06 20:34:13 +0200

    Create Multiplication_table.cpp

---
+++ Assiut University Sheet/Loops/Multiplication_table.cpp
@@ -0,0 +1,19 @@
+/*
+Author: Karim Tarek Ibrahim
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int N;
+	cin >> N;
+	for (int i = 1; i <= 12; i++)
+	{
+		cout << N << " * " << i << " = " << N * i << endl;
+	}
+	
+
+	return 0;
+}
