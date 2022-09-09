8e2f2dcf0d71de1a6bd5d6eb4280e7c6586d5016
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2022-09-09 23:21:35 +0200

    Create Timon_and_Pumbaa.cpp

---
+++ Assiut University Sheet/Contest 2/Timon_and_Pumbaa.cpp
@@ -0,0 +1,21 @@
+/*
+Author: Karim Tarek Ibrahim
+*/
+
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+	
+	long long a, b;
+	cin >> a >> b;
+	if ((a - b) >= 0)
+		cout << a - b;
+	else
+		cout << 0;
+ 
+ 
+	return 0;
+}
+ 
