7317cbff163721fcb0887cfcdb85ea4f412f228f
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2022-09-18 12:08:36 +0200

    Update Mini-Max Sum.cpp

--- HackerRank/Prepare/Problem Solving/Algorithms/Mini-Max Sum.cpp
+++ HackerRank/Prepare/Problem Solving/Algorithms/Mini-Max Sum.cpp
@@ -7,51 +7,45 @@ Date: 18/9/2022
 #include <bits/stdc++.h>
 using namespace std;
 
-
-void miniMaxSum(int arr[]) {
-    int mn = 0, mx = 0;
-    bool flag_mn = true, flag_mx = true;
-    for (int i = 0; i < 5; i++)
+void miniMaxSum(long long arr[]) {
+    long long mn=0, mx=0;
+    bool flag_mn=true, flag_mx=true;
+    for(int i=0; i<5; i++)
     {
-        int sum = 0;
-        for (int j = 0; j < 5; j++)
+        long long sum=0;
+        for(int j=0; j<5; j++)
         {
-            sum += arr[j];
+            sum+=arr[j];
         }
-        sum -= arr[i];
-        //determine ethe minimum
-        if (flag_mn)
+        sum-=arr[i];
+        if(flag_mn)
         {
-            mn = sum;
-            flag_mn = false;
+            mn=sum;
+            flag_mn=false;
         }
-        if (sum < mn)
-            mn = sum;
-
-        //determine the maximum
-        if (flag_mx)
+        if(sum<mn)
+        mn=sum;
+        if(flag_mx)
         {
-            mx = sum;
-            flag_mx = false;
+            mx=sum;
+            flag_mx=false;
         }
-        if (sum > mx)
-            mx = sum;
+        if(sum>mx)
+        mx=sum;
     }
-    cout << mn << " " << mx;
+    cout<<mn<<" "<<mx;
 }
 
 int main()
 {
-    int A[5];
-    for (int i = 0; i < 5; i++)
+    long long A[5];
+    for(int i=0; i<5; i++)
     {
-        cin >> A[i];
+        cin>>A[i];
     }
     miniMaxSum(A);
-
+    
 
     return 0;
 }
-
-
 
