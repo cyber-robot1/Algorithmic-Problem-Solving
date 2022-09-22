d6fc811b87fb95376f54cc797b320ae5a2caf942
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2022-09-22 09:21:21 +0200

    Create Plus Minus.cpp

---
+++ HackerRank/Prepare/Problem Solving/Algorithms/Plus Minus.cpp
@@ -0,0 +1,38 @@
+/*
+Author: Karim Tarek Ibrahim
+Problem link: https://www.hackerrank.com/challenges/plus-minus/problem
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void plusMinus(int arr[], int n) {
+    
+    double n_p = 0, n_n = 0, n_0 = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > 0)
+            n_p++;
+        else if (arr[i] < 0)
+            n_n++;
+        else
+            n_0++;
+    }
+    cout << fixed << setprecision(6) << n_p / n << endl;
+    cout << fixed << setprecision(6) << n_n / n << endl;
+    cout << fixed << setprecision(6) << n_0 / n << endl;
+
+}
+
+int main()
+{
+    int n, arr[100];
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    plusMinus(arr, n);
+
+    return 0;
+}
