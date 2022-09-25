7ca73facb8f6337f013bbab18d572d254cf7b8fa
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2022-09-25 15:46:48 +0200

    Create Subarray Division.cpp

---
+++ HackerRank/Prepare/Problem Solving/Algorithms/Implementation/Subarray Division.cpp
@@ -0,0 +1,45 @@
+/*
+Author: Karim Tarek Ibrahim
+Problem link: https://www.hackerrank.com/challenges/the-birthday-bar/problem
+*/
+
+#include <bits/stdc++.h> 
+using namespace std;
+
+int birthday(int s[], int n, int d, int m) {
+    
+    int sum = 0;         // (sum) => sum of elements of each subarray which equal to d.
+    int c = 0;          // (c) => num of elements of each subarray which equal to m.
+    int counter = 0;   // (counter) => num of valid subarrays.
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            for (int k = i; k <= j; k++)
+            {
+                sum += s[k];
+                c++;
+            }
+            if (m == c && d == sum)
+                counter++;
+            sum = 0;
+            c = 0;
+        }
+    }
+    return counter;
+}
+
+int main()
+{
+    int n, a[100], d, m;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    cin >> d >> m;
+
+    cout << birthday(a, n, d, m);
+
+	return 0;
+}
