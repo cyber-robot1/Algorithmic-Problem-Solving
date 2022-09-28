7b73bb063b31d29da797b1f7109f8a462034f3ae
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2022-09-28 14:26:20 +0200

    Update 1. Two Sum.cpp

--- LeetCode/Array/1. Two Sum.cpp
+++ LeetCode/Array/1. Two Sum.cpp
@@ -7,7 +7,7 @@ class Solution {
 public:
 	vector<int> twoSum(vector<int>& nums, int target) {
 		int a = 0, b = 0;
-        for (int i = 0; i < nums.size()-1; i++)
+		for (int i = 0; i < nums.size()-1; i++)
 		{
 			for (int j = i + 1; j < nums.size(); j++)
 			{
