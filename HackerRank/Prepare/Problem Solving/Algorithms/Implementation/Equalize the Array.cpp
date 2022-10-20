1b10ec214c83aed52bd7c776bd32e2b9dff60d5a
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2022-10-21 00:11:01 +0200

    Create Equalize the Array.cpp

---
+++ HackerRank/Prepare/Problem Solving/Algorithms/Implementation/Equalize the Array.cpp
@@ -0,0 +1,77 @@
+/*
+Author: Karim Tarek Ibrahim
+Problem link: https://www.hackerrank.com/challenges/equality-in-a-array/problem/
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int MaxinArray(int arr[], int n)
+{
+    int mx = 0;
+    mx = arr[0];
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] >= mx)
+            mx = arr[i];
+    }
+    return mx;
+}
+
+int MininArray(int arr[], int n)
+{
+    int mn;
+    mn = arr[0];
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] <= mn)
+            mn = arr[i];
+    }
+    return mn;
+}
+
+int MaxinFreqArray(int arr[], int n)
+{
+    int freq[105] = { 0 };
+    int mx = 0;
+    for (int i = 0; i < n; i++)
+        freq[arr[i]]++;
+
+    int start = arr[0];
+    int end = arr[n - 1];
+
+    mx = freq[start];
+    for (int i = start; i <= end; i++)
+    {
+        if (freq[i] > mx)
+            mx = freq[i];
+    }
+    return mx;
+}
+
+int SizeFreqArray(int arr[], int n)
+{
+    int mx = MaxinArray(arr, n);
+    int mn = MininArray(arr, n);
+    int m = (mx - mn) + 1;
+    return m;
+}
+
+int equalizeArray(int arr[], int n) {
+    int mx=MaxinFreqArray(arr, n);        // max element in frequency array.
+    int output=n-mx;
+    return output;
+}
+
+int arr[105];
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    sort(arr, arr + n);   
+    cout << equalizeArray(arr, n);
+    
+    return 0;
+}
