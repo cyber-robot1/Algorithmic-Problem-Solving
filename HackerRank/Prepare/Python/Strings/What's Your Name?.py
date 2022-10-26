257096ce2b78229de0ff473955657fbd7e18a5a3
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2022-10-26 23:29:52 +0200

    Create What's Your Name?.py

---
+++ HackerRank/Prepare/Python/Strings/What's Your Name?.py
@@ -0,0 +1,10 @@
+# Author: Karim Tarek Ibrahim
+# Problem link: https://www.hackerrank.com/challenges/whats-your-name/problem
+
+def print_full_name(first, last):
+    print("Hello", first, last+"!", "You just delved into python.")
+
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
