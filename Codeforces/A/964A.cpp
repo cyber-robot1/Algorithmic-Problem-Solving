9929bb311bacb7c4d96db34a2c8921d5f2942adc
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2023-02-25 02:28:27 +0200

    Create 964A.cpp

---
+++ Codeforces/A/964A.cpp
@@ -0,0 +1,20 @@
+/*
+Author: Karim Tarek Ibrahim
+Problem name: Splits
+Problem link: https://codeforces.com/problemset/problem/964/A
+Date: 30/8/2022
+*/
+
+#include <iostream>
+using namespace std;
+ 
+ 
+int main()
+{
+	long long n;
+	cin >> n;
+	cout << (n / 2) + 1;
+	
+			
+	return 0;
+}
