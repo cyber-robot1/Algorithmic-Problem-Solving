d2427464ae2a6e720486baa2aa344ef1bca2f0a9
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2023-04-26 13:03:24 +0200

    Create 389A.cpp

---
+++ Codeforces/A/389A.cpp
@@ -0,0 +1,65 @@
+/*
+Author: Karim Tarek Ibrahim
+Problem name: Fox and Number Game
+Problem link: https://codeforces.com/problemset/problem/389/A
+Date: 26/4/2023
+*/
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+ 
+bool EqualArr(int n, int arr[])
+{
+    bool flag = true;
+    for (int i = 0; i < n; i++)
+    {
+        if (i == n - 1)
+            break;
+        else
+        {
+            if (arr[i] == arr[i + 1])
+                flag = true;
+            else
+            {
+                flag = false;
+                break;
+            }
+        }
+    }
+    return flag;
+}
+ 
+int arr[101];
+int main()
+{
+    
+    int n, sum = 0;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+ 
+    sort(arr, arr + n);
+ 
+    while (!(EqualArr(n, arr)))
+    {
+        for (int i = n - 1; i >= 0; i--)
+        {
+            if (i == 0)
+                break;
+            else
+            {
+                if (arr[i] != arr[i - 1])
+                    arr[i] = arr[i] - arr[i - 1];
+            }
+        }
+        sort(arr, arr + n);
+    }
+ 
+    for (int i = 0; i < n; i++)
+        sum += arr[i];
+ 
+    cout << sum;
+ 
+    return 0;
+}
