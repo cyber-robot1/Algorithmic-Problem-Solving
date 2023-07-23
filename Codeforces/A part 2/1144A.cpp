3668d7de726723b23871b4c59ac4f5a423a1d08b
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2023-07-23 10:13:43 +0300

    Create 1144A.cpp

---
+++ Codeforces/A part 2/1144A.cpp
@@ -0,0 +1,61 @@
+/*
+Author: Karim Tarek Ibrahim
+Problem name: Diverse Strings
+Problem link: https://codeforces.com/problemset/problem/1144/A
+Date: 23/7/2023
+*/
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int freq[150];
+int main()
+{
+
+	int n;
+	cin >> n;
+	while (n--)
+	{
+		string s;
+		bool f1 = true, f2 = true;
+		cin >> s;
+		sort(s.begin(), s.end());
+
+		for (int i = 0, j = s[0]; i < s.size(); i++, j++)
+		{
+			if (s[i] == (char)j)
+				continue;
+			else
+			{
+				f1 = false;
+				break;
+			}
+		}
+
+		for (int i = 0; i < s.size(); i++)
+			freq[s[i]]++;
+
+		for (int i = s[0]; i <= s[s.size() - 1]; i++)
+		{
+			if (freq[i] > 1)
+			{
+				f2 = false;
+				break;
+			}
+		}
+
+		if (f1 && f2)
+			cout << "YES" << endl;
+		else
+			cout << "NO" << endl;
+
+
+		for (int i = s[0]; i <= s[s.size() - 1]; i++)
+			freq[i] = 0;
+
+	}	
+
+	return 0;
+}
