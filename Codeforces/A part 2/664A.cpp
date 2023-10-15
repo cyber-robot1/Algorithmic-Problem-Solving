f3e49181a0f842ce9f4415bd937ace67df3bcfae
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2023-10-15 19:34:39 +0300

    Create 664A.cpp

---
+++ Codeforces/A part 2/664A.cpp
@@ -0,0 +1,22 @@
+/*
+Author: Karim Tarek Ibrahim
+Problem name: Complicated GCD
+Problem link: https://codeforces.com/problemset/problem/664/A
+Date: 15/10/2023
+*/
+
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+	string s1, s2;
+	cin >> s1 >> s2;
+	if (s1 == s2)
+		cout << s1;
+	else
+		cout << 1;
+ 
+	return 0;
+}
+
