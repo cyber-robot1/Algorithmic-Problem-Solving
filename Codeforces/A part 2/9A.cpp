f4dbec4149eeaf3fc61ecf4b6bd72319ba01b37d
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2023-10-25 17:43:59 +0300

    Create 9A.cpp

---
+++ Codeforces/A part 2/9A.cpp
@@ -0,0 +1,32 @@
+/*
+Author: Karim Tarek Ibrahim
+Problem name: Die Roll
+Problem link: https://codeforces.com/problemset/problem/9/A
+Date: 25/10/2023
+*/
+
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+	int A, B;
+	cin >> A >> B; 
+	A = max(A, B);
+	A = (6 - A) + 1;
+	B = 6;
+ 
+	if (A == 4)
+		cout << 2 << "/" << 3;
+	else
+	{
+		while (B % A == 0 && A != 1)
+		{
+			B /= A;
+			A /= A;
+		}
+		cout << A << "/" << B;
+	}
+  
+	return 0;
+}
