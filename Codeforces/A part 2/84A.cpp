33485b866cf8953e9e3b1fd35c7924a42acd0c5a
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2023-10-29 23:48:30 +0200

    Create 84A.cpp

---
+++ Codeforces/A part 2/84A.cpp
@@ -0,0 +1,17 @@
+/*
+Author: Karim Tarek Ibrahim
+Problem name: Toy Army
+Problem link: https://codeforces.com/problemset/problem/84/A
+Date: 29/10/2023
+*/
+
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+	int n; cin >> n;
+	cout << (n / 2) * 3;
+ 
+	return 0;
+}
