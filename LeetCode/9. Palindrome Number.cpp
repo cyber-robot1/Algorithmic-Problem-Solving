2727565fa6ee0c41c22c4bf338177cc6ad9a1aa6
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2025-02-03 18:20:33 +0200

    Create 9. Palindrome Number.cpp

---
+++ LeetCode/9. Palindrome Number.cpp
@@ -0,0 +1,24 @@
+/*
+Author: Karim Tarek Ibrahim
+Problem link: https://leetcode.com/problems/palindrome-number/
+*/
+
+class Solution {
+public:
+	bool isPalindrome(int x) {
+		long long digit, rev_num = 0, temp;
+		temp = x;
+		while (temp)
+		{
+			digit = temp % 10;
+			temp = temp / 10;
+			rev_num = (rev_num * 10) + digit;
+		}
+		if (x < 0)
+			return false;
+		if (x == rev_num)
+			return true;
+        else
+            return false;
+	}
+}
