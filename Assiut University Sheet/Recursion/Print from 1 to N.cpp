71a286125cf00bfa5456d4a91749f5865aae92ec
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2026-02-05 17:17:48 +0200

    Create Print from 1 to N.cpp

---
+++ Assiut University Sheet/Recursion/Print from 1 to N.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void print(int n)
+{
+    if (n == 1)
+    {
+        cout << n << endl;
+        return;
+    }
+    print(n - 1);
+    cout << n << endl;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    print(n);
+
+    return 0;
+}
